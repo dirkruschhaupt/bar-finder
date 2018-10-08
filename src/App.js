f05dfd9e71b64575b4bd898ead5b1215a72ce502
dirk ruschhaupt <dirkruschhaupt@gmail.com>
2018-10-08 11:07:41 -0500

    minor corrections

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bar: "Click to find a bar:"
+      bar: ""
     }
   }
 
@@ -21,11 +21,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1>Find a Bar</h1>
+          <p>Click the button below to find a bar</p>
           <button onClick={() => this.generateBar()}>Find</button>
         </div>
         <p className="App-intro">
           <Bar bar={this.state.bar} />
         </p>
+        <footer>
+          <div>Please Drink Responsibly, Don't Drink and Drive. Get an <a href="https://www.uber.com/">Uber</a> or <a href="https://www.lyft.com/">Lyft</a></div>
+        </footer>
       </div>
     );
   }
